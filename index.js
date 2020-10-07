4b8efe03b28cb9fca16b8fac14e40aa8e3ab03af
Ntare Guy <gntare2@gmail.com>
2020-10-07 08:05:07 +0200

    feat: sending emails with node.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const nodemailer = require('nodemailer');
 
 const app = express()
 
@@ -14,4 +15,33 @@ app.get('/watches', function (req, res) {
 })
 app.listen(3002, function() {
     console.info('Application is running locally on 3002')
+})
+
+app.get('/mail', function (req, res){
+    // Testing email
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "guyntare745@gmail.com",
+            pass: "test@123!"
+        }
+    });
+
+    const message = {
+        from: 'guyntare745@gmail.com',
+        to: 'samuel.nishimwe@andela.com',
+        subject: 'Sending email with node.js',
+        text: 'Good morning Samuel, I just sent you this email in node.js tbh this is lit :)'
+    };
+
+    transporter.sendMail(message, function(error, info){
+        if (error){
+            console.log(error);
+        }else{
+            res.send('Email sent: ' + info.response);
+            console.info("Email sent")
+        }
+    });
+
 })
