1202820d13b2c15eac132369e0925536321345d5
Kundwa Bruno <andela@Bruno.local>
2020-10-09 11:36:29 +0200

    Updated my branch with few changes

--- index.js
+++ index.js
@@ -17,17 +17,23 @@ app.get('/watches', function (req, res) {
 app.get('/country', (req,res) => {
     res.sendFile(__dirname + '/country.html');
 })
-app.post('/country', (req,res) => {
+app.post('/country', (req,res,error) => {
     let countryName = String(req.body.country);
-    let countryResult = lookup.byCountry(countryName);
-    res.set("Content-Type", "text/html");
-    res.write('<h3>Country Searched: ' + countryResult.country + '</h3>');
-    res.write("<h3>Continet:"  + countryResult.continent + '</h3>');
-    res.write("<h3>Region: " + countryResult.region + '</h3>');
-    res.write("<h3>Capital City: " + countryResult.capital + '</h3>');
-    res.write("<h3>Iso 2 code: " + countryResult.iso2 + '</h3>');
-    res.write("<h3>Iso 3 code: " + countryResult.iso3 + '</h3>');
-    res.send();
+    countryName = countryName.charAt(0).toUpperCase() + countryName.slice(1);
+    let countryResult = lookup.byCountry(countryName); 
+        try{
+        res.set("Content-Type", "text/html");
+        res.write('<h3>Country Searched: ' + countryResult.country + '</h3>');
+        res.write("<h3>Continet:"  + countryResult.continent + '</h3>');
+        res.write("<h3>Region: " + countryResult.region + '</h3>');
+        res.write("<h3>Capital City: " + countryResult.capital + '</h3>');
+        res.write("<h3>Iso 2 code: " + countryResult.iso2 + '</h3>');
+        res.write("<h3>Iso 3 code: " + countryResult.iso3 + '</h3>');
+        res.send();
+        }
+        catch(err){
+            res.send("Please enter a valid country name");
+        }
 })
 app.listen(3002, function() {
     console.info('Application is running locally on 3002')
